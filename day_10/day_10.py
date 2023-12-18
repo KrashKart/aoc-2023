a4278d06f7e3ed9df419cfdec274a2158f3a485f
KrashKart <xfiles123@gmail.com>
2023-12-18 16:47:51 +0800

    Successfully implemented and rewrote day 10 code

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,47 +1,53 @@
-import copy
 maze = []
 with open("day_10.txt", "r") as f:
     for idx, line in enumerate(f):
         if "S" in line:
             start = (idx, line.index("S"))
+            line = line.replace("S", "J")
         line = list(line.strip())
         maze.append(line)
 
-maze_copy = copy.deepcopy(maze)
+dirs = {"|": lambda di,dj: (di,dj), "-": lambda di,dj: (di,dj), 
+        "7": lambda di,dj: (dj,di), "F": lambda di,dj: (-dj,-di),
+        "L": lambda di,dj: (dj,di), "J": lambda di,dj: (-dj,-di)}
+
 def part_1():
-    maxi = 1
-    curr = (start[0] - 1, start[1])
-    prev = start
-    while maze[curr[0]][curr[1]] != "S":
-        match maze[curr[0]][curr[1]]:
-            case "|": nex = [(curr[0] - 1, curr[1]), (curr[0] + 1, curr[1])]
-            case "7": nex = [(curr[0] + 1, curr[1]), (curr[0], curr[1] - 1)]
-            case "J": nex = [(curr[0] - 1, curr[1]), (curr[0], curr[1] - 1)]
-            case "-": nex = [(curr[0], curr[1] - 1), (curr[0], curr[1] + 1)]
-            case "L": nex = [(curr[0] - 1, curr[1]), (curr[0], curr[1] + 1)]
-            case "F": nex = [(curr[0] + 1, curr[1]), (curr[0], curr[1] + 1)]
-        nex = list(filter(lambda x: x != prev, nex))[0]
-        maze_copy[curr[0]][curr[1]] = maxi
-        maxi += 1
-        prev = curr
-        curr = nex
-    print(maxi // 2 if maxi % 2 == 0 else maxi // 2 + 1)
+    visited = []
+    num = -1
+    i, j = start
+    di, dj = -1,0
+    while (i, j) not in visited:
+        visited.append((i, j))
+        i, j = i + di, j + dj
+        num += 1
+        di, dj = dirs[maze[i][j]](di, dj)
+    print(num / 2 if num % 2 == 0 else num // 2 + 1)
+
+def shoelace(verts):
+    iss, jss = [i for i, j in verts], [j for i, j in verts]
+    a, b = 0, 0
+    for c in range(1, len(jss)):
+        a += iss[c-1] * jss[c]
+        b += iss[c] * jss[c-1]
+    a += iss[-1] * jss[0]
+    b += iss[0] * jss[-1]
+    return 0.5 * abs(a - b)
 
 def part_2():
-    sum = 0
-    def count_nums(i, j):
-        sum_nums = 0
-        for jj in range(j):
-            if maze[i][jj] in "|JLS" and isinstance(maze_copy[i][jj], int):
-                sum_nums += 1
-        return sum_nums % 2 == 1
-
-    for i in range(1, len(maze)):
-        for j in range(1, len(maze[0])):
-            if not isinstance(maze_copy[i][j], int) and count_nums(i, j):
-                sum += 1
-    print(sum)
-
+    visited = []
+    verts = []
+    i, j = start
+    di, dj = -1,0
+    num = 0
+    while (i, j) not in visited:
+        visited.append((i, j))
+        if maze[i][j] in "J7FL":
+            verts.append((i, j))
+        i, j = i + di, j + dj
+        di, dj = dirs[maze[i][j]](di, dj)
+        num += 1
+    print(int(shoelace(verts) - num / 2 + 1))
+    
 if __name__ == "__main__":
     part_1()
     part_2()
